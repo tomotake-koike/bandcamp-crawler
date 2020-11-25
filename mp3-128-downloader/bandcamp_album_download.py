9d937206730101a83cc1bdb40a39299efa18b74b
Koike, Tomotake <tomotake.koike@gmail.com>
2020-11-26 05:24:31 +0900

    Refactering for new site formats

--- mp3-128-downloader/bandcamp_album_download.py
+++ mp3-128-downloader/bandcamp_album_download.py
@@ -2,84 +2,92 @@
 import json, os, requests, re
 from bs4 import BeautifulSoup
 
-start_point = re.compile( "^var\s+TralbumData\s*=\s*{" )
-end_point = re.compile( "^\}" )
-comment_part = re.compile( "(\/\/\s.+)$" )
-string_bonding = re.compile( '("\s*\+\s*")' )
-json_line = re.compile( "^\s*([^:\s]+)\s*:(.+)$" )
-path_query = re.compile( "[\/\*\$\;\<\>]" )
+path_query = re.compile( '[\/\*\$\;\<\>]' )
+track_href = re.compile( '^\s*\<a\shref=\"(\/(track)[^\"]+)\"' )
+album_href = re.compile( '^\s*\<a\shref=\"(\/(track|album)[^\"]+)\"' )
+url_separator = re.compile( '\/\/([^\.]+)\.' )
+track_searcher = re.compile( '\/(track)\/' )
 
 def download( url ):
     TralbumData = ""
-
-    m=re.search( '\/\/(\w+)\.', url )
+    scripts = []
+    m=url_separator.search( url )
     group = m.group(1)
     gpath = path_query.sub( "_", group )
     if not os.path.isdir( gpath ) : os.mkdir( gpath )
 
-    with requests.get( url ) as res:
 
+    with requests.get( url ) as res:
         soup = BeautifulSoup( res.content, "html.parser" )
         scripts = soup.find_all("script")
+        for scripts_index in range( 3, len( scripts ) ) :
+            if scripts[ scripts_index ].has_attr("data-tralbum"):
+                TralbumData = scripts[ scripts_index ]["data-tralbum"]
+                break
+            else:
+                TralbumData = ""
+            if len( TralbumData ) > 0 : break
         
-        for l in scripts[10].string.splitlines() :
-            # l = l.decode()
-            if start_point.search( l ) : TralbumData = "{"
-            if TralbumData :
-                l = comment_part.sub( "", l )
-                if end_point.search( l ) :
-                    TralbumData += "}"
-                    break
-                m = json_line.search( l )
-                if m :
-                    TralbumData += '"' + m.group( 1 ) + '": ' + string_bonding.sub( "", m.group( 2 ) )
+    try:
+        j = json.loads( TralbumData )
+        if j:
+            artist = "anonimous"
+            if j["artist"] : artist = j["artist"]
+            path = gpath + "/" + path_query.sub( "_", artist )
+            if not os.path.isdir( path ) : os.mkdir( path )
+            
+            album = "no title"
+            if j["packages"]:
+                if j["packages"][0]["album_title"] : album = j["packages"][0]["album_title"]
+            else:
+                if j["current"] and j["current"]["title"] : album = j["current"]["title"]
 
-    j = json.loads( TralbumData )
-    if j:
-        artist = "anonimous"
-        if j["artist"] : artist = j["artist"]
-        path = gpath + "/" + path_query.sub( "_", artist )
-        if not os.path.isdir( path ) : os.mkdir( path )
-    
-        album = "no title"
-        if j["packages"]:
-            if j["packages"][0]["album_title"] : album = j["packages"][0]["album_title"]
-        else:
-            if j["current"] and j["current"]["title"] : album = j["current"]["title"]
+            out_dir = path + "/" + path_query.sub( "_", album )
+            if not os.path.isdir( out_dir ) : os.mkdir( out_dir )
 
-        out_dir = path + "/" + path_query.sub( "_", album )
-        if not os.path.isdir( out_dir ) : os.mkdir( out_dir )
-    
-#        print( "Downloading.. : " + path_query.sub( "", artist + "/" + album ) )
+            for t in j["trackinfo"]:
+                track_num = 0
+                if t['track_num'] : track_num = t['track_num']
+                out =  out_dir + "/" + path_query.sub( "_", "{0:02d}_".format( track_num ) + t['title'] + ".mp3" )
+                if not os.path.exists( out ):
+                    if t['file']:
+                        print( "Downloading ... : " + out )
+                        mp3_url = t['file']['mp3-128']
+                        with requests.get( mp3_url ) as res:
+                            with open( out, 'wb' ) as of:
+                                of.write( res.content )
 
-        for t in j["trackinfo"]:
-            track_num = 0
-            if t['track_num'] : track_num = t['track_num']
-            out =  out_dir + "/" + path_query.sub( "_", "{0:02d}_".format( track_num ) + t['title'] + ".mp3" )
-            if not os.path.exists( out ):
-                if t['file']:
-                    print( "Downloading ... : " + out )
-                    mp3_url = t['file']['mp3-128']
-                    with requests.get( mp3_url ) as res:
-                        with open( out, 'wb' ) as of:
-                            of.write( res.content )
+    except Exception as e:
+        print ( e )
+        print ("JSON Decode error")
+        print ( url )
+        print ( scripts[ scripts_index ]   )
 
 
-
-
+def album_download( url ):
+    with requests.get( url ) as res:
+        for l in res.content.splitlines() :
+            l = l.decode()
+            m = track_href.search( l )
+            if m:
+                if m.group( 1 ).count('?') == 0:
+                    a = url.split( '/' )
+                    print( a[0] + '//' + a[2] + m.group( 1 ) )
+                    download( a[0] + '//' + a[2] + m.group( 1 ) )
+        
 if __name__ == '__main__':
-    
-    url = os.sys.argv[ 1 ]
-    album_href = re.compile( '^\s*\<a\shref=\"(\/(track|album)[^\"]+)\"' )
-    if re.search( "\/(album|track)\/" , url ) :
-        download( url )
-    else:
-       with requests.get( url ) as res:
-           for l in res.content.splitlines() :
-               l = l.decode()
-               m = album_href.search( l )
-               if m:
-                   download( url + m.group( 1 ) )
 
+    for url in os.sys.argv[ 1: ]:
+        if track_searcher.search( url ) :
+            download( url )
+        else:
+            with requests.get( url ) as res:
+                for l in res.content.splitlines() :
+                    l = l.decode()
+                    m = album_href.search( l )
+                    if m:
+                        if m.group( 1 ).count('?') == 0:
+                            a = url.split( '/' )
+                            print( a[0] + '//' + a[2] + m.group( 1 ) )
+                            album_download( a[0] + '//' + a[2] + m.group( 1 ) )
                    
-               
